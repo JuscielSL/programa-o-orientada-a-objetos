52ebe0ec2b02f91d66c9f2c002706781c100ce7e
Jusciel <juscieldasilvalopes42@gmail.com>
2026-03-19 10:10:14 -0300

    Add files via upload

---
+++ Atividade.java
@@ -0,0 +1,28 @@
+public class atividade {
+    private double numero1;
+    private double numero2;
+
+    double soma(Double numero1, Double numero2) {
+        return numero1 + numero2;
+    }  
+
+    double subtracao(double numero1, double numero2) {
+        return numero1 - numero2;
+    }   
+
+    double multiplicacao(double numero1, double numero2) {
+        return numero1 * numero2;
+    }    
+
+    double divisao(double numero1, double numero2) {
+        return numero1 / numero2;
+    }
+    double resto(double numero1, double numero2) {
+        return numero1 % numero2;
+    }
+    double porcentagem(double numero1, double numero2) {
+        return (numero1 * numero2) / 100;
+    }  
+}
+    
+
