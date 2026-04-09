6789b231ba9146ecb97984843d75d49a2523b125
Jusciel <juscieldasilvalopes42@gmail.com>
2026-04-09 11:44:44 -0300

    Criar gerente.java

---
+++ gerente.java
@@ -0,0 +1,19 @@
+// Classe filha Gerente (especializado)
+// Herança da classe Funcionario
+// a propriedade extends faz relacionamento
+// de herança entre Gerente e Funcionario
+
+public class Gerente extends Funcionário { 
+    // método construtor que irá acessar a classe pai
+    public Gerente (String nome, Double salario){
+        // enviando os valores para o metodo construtor 
+        // da classe pai Funcionario
+        super(nome, salario);
+    }
+    // criaremos um método público para incrementar
+    // o salario protegido (protected) da classe pai
+    public void bonusSalarial(){
+        this.salarioFuncionario = 500.00;
+    }
+    
+}
