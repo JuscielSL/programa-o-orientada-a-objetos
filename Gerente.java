af48d1f529101e259a50bfe44478230a8bccc038
Jusciel <juscieldasilvalopes42@gmail.com>
2026-04-16 08:56:06 -0300

    Update and rename gerente.java to Gerente.java

--- Gerente.java
+++ Gerente.java
@@ -3,9 +3,9 @@
 // a propriedade extends faz relacionamento
 // de herança entre Gerente e Funcionario
 
-public class gerente extends Funcionário { 
+public class Gerente extends Funcionário { 
     // método construtor que irá acessar a classe pai
-    public gerente (String nome, Double salario){
+    public Gerente (String nome, Double salario){
         // enviando os valores para o metodo construtor 
         // da classe pai Funcionario
         super(nome, salario);
