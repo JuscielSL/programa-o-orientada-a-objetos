ffa6c1a09aae3feaa855c49f2068590e8492e714
Jusciel <juscieldasilvalopes42@gmail.com>
2026-04-16 08:53:20 -0300

    Update funcionario.java

--- funcionario.java
+++ funcionario.java
@@ -1,1 +1,21 @@
 
+/**
+ * Exercicio 3. Nível intermediario: Modificador Protected
+ * Permitir acesso apenas para classes filhas (heranças)
+ * Se o salario fosse private, o Gerente não
+ * conseguiria altera-lo diretamente
+ */
+
+public class funcionario {
+    public String nomeFuncionario; // Publico: qualquer
+    // Protected: visivel para a classe e suas subclasses
+    protected Double salarioFuncionario;
+    // método construtor público desta classe
+    public funcionario (String nome, Double salario) {
+        this.nomeFuncionario = nome;
+        this.salarioFuncionario = salario;
+    }
+
+    
+    
+}
