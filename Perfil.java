de700108c0df99eb7298776bc486dbc378bbcf9b
Jusciel <juscieldasilvalopes42@gmail.com>
2026-04-23 11:22:42 -0300

    Update Perfil.java

--- Perfil.java
+++ Perfil.java
@@ -7,16 +7,18 @@
  */
 
 public class Perfil {
-    //Declarando uma propriedade encapsulada
-    private String email = ""; 
-    //criando um metodo para alterar o email
-    public void setEmail(String emailInformado){
-        //Encapsula a logica de validação do email
-        if (email.contains("@")&& email.contains(".")){
+    private String email = "";
+
+    public void setEmail(String emailInformado) {
+        if (emailInformado.contains("@") && emailInformado.contains(".")) {
             this.email = emailInformado;
             System.out.println("E-mail atualizado!");
-    } else {
-            System.out.println("E-mail inválido! O email ");
+        } else {
+            System.out.println("E-mail inválido!");
         }
+    }
+
+    public String getEmail() {
+        return this.email;
     }
 }
