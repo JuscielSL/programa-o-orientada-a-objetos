d17a176b2eb38721506108219a91582be6cbb636
Jusciel <juscieldasilvalopes42@gmail.com>
2026-05-21 10:49:56 -0300

    Delete EmailNotificacao.java

--- EmailNotificacao.java
+++
@@ -1,9 +0,0 @@
-package exercicio2;
-
-public class EmailNotificacao implements Notificacao {
-    @Override
-    public void enviar(String mensagem) {
-        System.out.println("Enviando email: " + mensagem);
-    }
-    
-}
