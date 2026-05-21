d1f0179fc90be810f15709472acb841c2088c69c
Jusciel <juscieldasilvalopes42@gmail.com>
2026-05-21 10:51:21 -0300

    Delete TesteMainPagamento.java

--- TesteMainPagamento.java
+++
@@ -1,8 +0,0 @@
-package exercicio1;
-
-public class TesteMainPagamento {
-    public static void main(String[] args) {
-        Pagamento pagamento = new PagamentoCartao();
-        pagamento.processar(500.00); 
-    }
-}
