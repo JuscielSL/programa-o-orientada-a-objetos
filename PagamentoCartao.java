2a8af0ed9d0df537c69cc083e7d55ce91294c316
Jusciel <juscieldasilvalopes42@gmail.com>
2026-05-21 10:50:40 -0300

    Delete PagamentoCartao.java

--- PagamentoCartao.java
+++
@@ -1,8 +0,0 @@
-package exercicio1;
-// Especialização para cartão de credito
-public class PagamentoCartao extends Pagamento {
-    @Override // anotação de sobrescrita
-    protected double calculaTaxa(double valor) {
-        return valor * 0.05; // 5% de taxa da maquininha
-    }
-}
