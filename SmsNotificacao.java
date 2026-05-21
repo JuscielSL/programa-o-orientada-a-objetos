7bc6ddbbf024d24ddc9f1f1c38d8854072bc078c
Jusciel <juscieldasilvalopes42@gmail.com>
2026-05-21 10:50:29 -0300

    Delete SmsNotificacao.java

--- SmsNotificacao.java
+++
@@ -1,9 +0,0 @@
-package exercicio2;
-
-public class SmsNotificacao implements Notificacao {
-    @Override
-    public void enviar(String mensagem) {
-        System.out.println("Enviando SMS: " + mensagem);
-    }
-    
-}
